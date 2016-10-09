6245a0dd2498a006033eab55ea1822c512a1c17b
Andrew Yang <andrewyang96@gmail.com>
2016-10-08 22:07:19 -0500

    Start on app

--- epapp/app.py
+++ epapp/app.py
@@ -1,9 +1,17 @@
 """Entry point for EarthPron app."""
 from flask import Flask
+from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    print 'Hello world'
+    return render_template('index.html',
+                           stylesheet=url_for('static', filename='style.css'),
+                           script=url_for('static', filename='script.js'))
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
