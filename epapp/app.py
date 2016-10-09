ab03db4b94308f5ce36cb3a41fd15de4afabae2f
Andrew Yang <andrewyang96@gmail.com>
2016-10-08 22:45:41 -0500

    Fix errors in sqlite3 statement

--- epapp/app.py
+++ epapp/app.py
@@ -1,9 +1,29 @@
 """Entry point for EarthPron app."""
+
+import datetime
+import sqlite3
+import time
+
 from flask import Flask
+from flask import g
+from flask import jsonify
 from flask import render_template
 from flask import url_for
 
 app = Flask(__name__)
+DATABASE = 'earthpron.db'
+ONE_WEEK = 604800
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+def get_current_time():
+    return int(time.mktime(datetime.datetime.utcnow().timetuple()))
 
 
 @app.route('/')
@@ -11,6 +31,22 @@ def index():
     return render_template('index.html',
                            stylesheet=url_for('static', filename='style.css'),
                            script=url_for('static', filename='script.js'))
+
+
+@app.route('/data')
+def data():
+    c = get_db()
+    results = c.execute('SELECT * FROM hot_posts WHERE created_utc>? LIMIT 25',
+                        (get_current_time() - ONE_WEEK,)).fetchall()
+    c.close()
+    return jsonify({'results': results})
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 if __name__ == '__main__':
