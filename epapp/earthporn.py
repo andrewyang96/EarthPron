b25ac98cf0fd0ea9052c7190c7dee5d3444d0531
Andrew Yang <andrewyang96@gmail.com>
2016-10-08 20:53:04 -0500

    Finish refactor of util functions

--- epapp/earthporn.py
+++ epapp/earthporn.py
@@ -20,43 +20,44 @@ import pyimgur
 
 from subreddits import known_subreddits
 
-r = praw.Reddit(user_agent="earthporn_maps")
+r = praw.Reddit(user_agent='earthpron_rocks')
 h = HTMLParser.HTMLParser()
 imgur = pyimgur.Imgur(IMGUR_API_KEY)
 alchemyapi = AlchemyAPI()
 ACCEPTED_ENTITY_TYPES = [
-    "City",
-    "Company",
-    "Continent",
-    "Country",
-    "GeographicFeature",
-    "Organization",
-    "Region",
-    "StateOrCounty"
+    'City',
+    'Company',
+    'Continent',
+    'Country',
+    'GeographicFeature',
+    'Organization',
+    'Region',
+    'StateOrCounty'
 ]
 
 
 def get_hot_posts(limit=10):
     """Fetch the hottest posts. Limit defaults to 10."""
-    multireddit = r.get_multireddit("theyangmaster", "earthporns")
-    hot_submissions = multireddit.get_hot(limit=limit)
+    multireddit = r.get_multireddit('theyangmaster', 'earthporns')
+    hot_submissions = list(multireddit.get_hot(limit=limit))
     for item in hot_submissions:
         item.title = h.unescape(item.title)
     return hot_submissions
 
 
 def get_entities_from_phrase(phrase):
-    """Fetch entity words from phrase via AlchemyAPI."""
-    res = alchemyapi.entities("text", phrase)
+    """Fetch entity objects from phrase via AlchemyAPI."""
+    res = alchemyapi.entities('text', phrase)
     if res['status'] == 'ERROR':
-        print "There has been an error. Printing response here:"
+        print 'There has been an error. Printing response here:'
         print res
         return []
     return filter(lambda entity: entity['type'] in ACCEPTED_ENTITY_TYPES,
                   res['entities'])
 
 
-def parse_search_query(entity_list):
+def transform_entities_to_words(entity_list):
+    """Transform entity objects into words."""
     ret = []
     for entity in entity_list:
         if 'disambiguation' in entity.keys():
@@ -67,8 +68,9 @@ def parse_search_query(entity_list):
 
 
 def get_coordinates(location):
-    url = "https://maps.googleapis.com/maps/api/geocode/json"
-    params = {"address": location, "key": GOOGLE_API_KEY}
+    """Fetch coordinates from a phrase of entity words."""
+    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    params = {'address': location, 'key': GOOGLE_API_KEY}
 
     url_parts = list(urlparse.urlparse(url))
     query = dict(urlparse.parse_qsl(url_parts[4]))
@@ -88,102 +90,71 @@ def get_coordinates(location):
     return j["results"][0]["geometry"]["location"]
 
 
-def get_url(url):
+def extract_image_url(url):
+    """Try to extract image URL from URL."""
     urlcomponents = urlparse.urlparse(url)
     netloc = urlcomponents[1]
-    if netloc == "www.flickr.com":
-        print "The URL is a Flickr URL. Changing..."
+    if netloc == 'www.flickr.com':
+        print 'The URL is a Flickr URL. Changing...'
         connection = urllib2.urlopen(url)
         soup = BeautifulSoup(connection)
         connection.close()
-        # Find #image-src tag and take its href attribute
-        return soup.find(id="image-src")["href"] # NOTE: sometimes there's a TypeError
-    elif netloc == "imgur.com":
+        return soup.find(id='image-src')['href']
+        # NOTE: sometimes there's a TypeError
+    elif netloc == 'imgur.com':
         old_path = urlcomponents[2]
-        if "a/" in old_path:
-            print "The URL is an Imgur album URL. Changing..."
+        if 'a/' in old_path:
+            print 'The URL is an Imgur album URL. Changing...'
             pathcomponents = old_path.split('/')
             album = imgur.get_album(pathcomponents[-1])
             return album.images[0].link
         else:
-            print "The URL is a plain Imgur URL. Changing..."
-            # Change netloc to "i.imgur.com" and append ".jpg" to path
-            urlcomponents = urlcomponents._replace(netloc="i.imgur.com")
-            urlcomponents = urlcomponents._replace(path=old_path+".jpg")
+            print 'The URL is a plain Imgur URL. Changing...'
+            urlcomponents = urlcomponents._replace(netloc='i.imgur.com')
+            urlcomponents = urlcomponents._replace(path=old_path + '.jpg')
             return urlparse.urlunparse(urlcomponents)
     else:
         return url
 
 
-def get_data(limit):
-    print "Time of execution:", datetime.datetime.now()
-    print "Limit:", limit
-    ret = []
-    print "Fetching Reddit posts"
-    print "-"*80
-    for post in get_hot_posts(limit):
-        print "Processing:", unicode(post.title).encode("utf-8"), ("-- from /r/" + post.subreddit.__str__())
-        append = []
-        entity_list = get_entities_from_phrase(post.title)
-        entities = parse_search_query(entity_list)
-        # append country name if necessary
-        if post.subreddit.__str__() in known_subreddits:
-            country = post.subreddit.__str__()
-            print "Found a country-related subreddit:", country
-            if country in entities:
-                print "Not necessary to add country to location string"
-            else:
-                print "Adding country to location string"
-                entities.append(known_subreddits[country])
-        search_query = " ".join(entities)
-        # find coords or else calls Google Reverse Geocoding API
-        coords = None
-        for entity in entity_list:
-            if 'disambiguation' in entity.keys() and 'geo' in entity['disambiguation'].keys():
-                coords = entity['disambiguation'].split(' ')
-                coords = {"lat": coords[0], "lng": coords[1]}
-                print "Geographics coords found with Alchemy API:", coords
-            else:
-                coords = get_coordinates(search_query)
-        if coords is not None:
-            try:
-                print unicode(search_query).encode("utf-8"), "has coords at", coords
-                imgurl = get_url(post.url)
-                html = ("<h3><a target='_blank' href='{0}'>{2}</a></h3>" +
-                        "<div><a target='_blank' href='http://www.reddit.com/r/{3}'>/r/{3}</a></div>" +
-                        "<img alt='{4}' src='{1}' class='featured-img'>").format(post.url, imgurl,
-                                                                                 unicode(post.title).encode("ascii", "xmlcharrefreplace"),
-                                                                                 post.subreddit.__str__(),
-                                                                                 unicode(search_query).encode("ascii", "xmlcharrefreplace"))
-                append.append(html)
-                append.append(coords["lat"])
-                append.append(coords["lng"])
-                ret.append(append)
-            except Exception as e:
-                print "Error fetching coordinates. Error printed below:"
-                print e
+def process_post(post):
+    """Process a post object received from get_hot_posts."""
+    print 'Processing:', unicode(post.title).encode('utf-8'), ('-- from /r/' + post.subreddit.__str__())
+    entity_objs = get_entities_from_phrase(post.title)
+    entity_words = transform_entities_to_words(entity_objs)
+
+    # append country name if necessary
+    if post.subreddit.__str__() in known_subreddits:
+        country = post.subreddit.__str__()
+        print 'Found a country-related subreddit:', country
+        if country in entity_words:
+            print 'Not necessary to add country to location string'
         else:
-            print "Cannot find coords for search query", search_query
-        print ""
-    print "Finished fetching Reddit posts"
-    return {"data": ret, "timestamp": datetime.datetime.utcnow().__str__() + "+0000"}
+            print 'Adding country to location string'
+            entity_words.append(known_subreddits[country])
+    query = ' '.join(entity_words)
+
+    # try to find coords in AlchemyAPI object
+    coords = get_coordinates(query)
+    if coords is None:
+        return None
+    else:
+        try:
+            print unicode(query).encode('utf-8'), 'has coords at', coords
+            img_url = extract_image_url(post.url)
+        except Exception as e:
+            print 'An error occurred'
+            print e
+        return {
+            'url': post.url,
+            'img_url': img_url,
+            'title': unicode(post.title).encode('ascii', 'xmlcharrefreplace'),
+            'subreddit': post.subreddit.__str__(),
+            'query': unicode(query).encode('ascii', 'xmlcharrefreplace')
+        }
 
 
 def test():
-    for post in get_hot_posts():
-        entity_list = get_entities_from_phrase(post.title)
-        entities = parse_search_query(entity_list)
-        if post.subreddit.__str__() in known_subreddits:
-            country = post.subreddit.__str__()
-            print "Found a country-related subreddit:", country
-            if country in entities:
-                print "Not necessary to add country to location string"
-            else:
-                print "Adding country to location string"
-                entities.append(known_subreddits[country])
-        search_query = " ".join(entities)
-        print "Title:", post.title
-        print "URL:", post.url
-        print "Subreddit:", post.subreddit
-        print "Search Query:", search_query
-        print ""
+    hot_posts = get_hot_posts()
+    results = filter(None, map(process_post, hot_posts))
+    print results
