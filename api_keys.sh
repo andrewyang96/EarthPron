79c53b9cdab70d17282be0b84c5a552c02e67909
Andrew Yang <andrewyang96@gmail.com>
2017-02-10 01:48:57 -0600

    Move api key output to earthpronupdater

--- api_keys.sh
+++ api_keys.sh
@@ -3,11 +3,11 @@
 read -p "Google API key: " google_api_key
 read -p "Imgur API key: " imgur_api_key
 
-echo {\"GOOGLE_API_KEY\": \"$google_api_key\", \"IMGUR_API_KEY\": \"$imgur_api_key\"} > earthpronapp/config.json
+echo {\"GOOGLE_API_KEY\": \"$google_api_key\", \"IMGUR_API_KEY\": \"$imgur_api_key\"} > earthpronupdater/config.json
 
 printf "config.json created\n"
 
 read -p "Alchemy API key: " alchemy_api_key
 
-python earthpronapp/alchemyapi.py $alchemy_api_key
-mv api_key.txt earthpronapp/api_key.txt
+python earthpronupdater/alchemyapi.py $alchemy_api_key
+mv api_key.txt earthpronupdater/api_key.txt
