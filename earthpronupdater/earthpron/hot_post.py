61b51621846644d5a18b5df131e60616fa7cf955
Andrew Yang <andrewyang96@gmail.com>
2017-02-11 00:01:02 -0600

    Refactor HotPost class

--- earthpronupdater/earthpron/hot_post.py
+++ earthpronupdater/earthpron/hot_post.py
@@ -1,4 +1,4 @@
-"""Class definition for a hot post."""
+"""Class definition for object representing Reddit posts."""
 
 
 class HotPost(object):
@@ -11,6 +11,11 @@ class HotPost(object):
         self.subreddit = subreddit
         self.created_utc = created_utc
 
+        self.keywords = None
+        self.lat = None
+        self.lng = None
+        self.image_url = None
+
     @classmethod
     def fetch_hot_posts_from_multireddit(cls, multireddit, limit=25):
         """Fetch hot posts from given multireddit."""
@@ -21,7 +26,7 @@ class HotPost(object):
             for item in hot_submissions
         ]
 
-    def commit_to_db(self, cur, keywords, lat, lng, image_url=None):
+    def commit_to_db(self, cur):
         """Insert self into given cursor's database.
 
         Returns False if there was conflict, else True.
@@ -30,6 +35,6 @@ class HotPost(object):
             INSERT INTO hot_posts
             (url, image_url, title, subreddit, query, created_utc, lat, lng)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING''', (
-            self.post_url, image_url, self.post_title, self.subreddit,
-            keywords, self.created_utc, lat, lng))
+            self.post_url, self.image_url, self.post_title, self.subreddit,
+            self.keywords, self.created_utc, self.lat, self.lng))
         return cur.rowcount
