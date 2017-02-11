4866a6f6afcfa362810d3adcde724d99efdfe6ae
Andrew Yang <andrewyang96@gmail.com>
2017-02-10 23:46:08 -0600

    Refactor geocode func

--- earthpronupdater/earthpron/geocode.py
+++ earthpronupdater/earthpron/geocode.py
@@ -4,13 +4,21 @@ import json
 import requests
 
 
-def get_coordinates(keywords, api_key):
-    """Fetch coordinates given an entity keywords string."""
+def get_coordinates(keywords, google_api_key):
+    """Fetch coordinates given an entity keywords string.
+
+    Returns None if Google's Geocoding API couldn't find coordinates.
+    Otherwise returns a tuple (lat, lng).
+    """
     url = 'https://maps.googleapis.com/maps/api/geocode/json'
-    params = {'address': keywords, 'key': api_key}
+    params = {'address': keywords, 'key': google_api_key}
 
     req = requests.get(url, params=params)
     j = json.loads(req.text)
     if j['status'] == 'ZERO_RESULTS':
         return None
-    return j['results'][0]['geometry']['location']
+
+    coords = j['results'][0]['geometry']['location']
+    if coords is None:
+        return None
+    return (coords['lat'], coords['lng'])
